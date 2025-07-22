687b0fc5e6a593eb998ca63a084eb5a42b0fa7c3
Kira Mishima <kiramishima@outlook.com>
2025-07-22 16:47:52 -0600

    Mise à jour du service de réduction avec EF Core
    Les modifications incluent la mise à jour de `Grpc.AspNetCore` à la version 2.71.0 et l'ajout de références pour `Mapster` et `Microsoft.EntityFrameworkCore.Sqlite`. Le service `GreeterService` a été remplacé par `DiscountService` dans `Program.cs`, et des paramètres de lancement ont été ajustés dans `launchSettings.json`.
    
    Le fichier `greet.proto` et le service associé ont été supprimés, tandis qu'une chaîne de connexion pour SQLite a été ajoutée dans `appsettings.json`. Des modifications ont également été apportées aux fichiers de configuration Docker pour intégrer le nouveau service `discount.grpc`. Enfin, la structure de la base de données a été mise en place pour gérer les coupons via le nouveau contexte `DiscountContext`.

---
+++ src/Services/Discount/Discount.Grpc/Data/Extentions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extentions
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>(); // obtenemos el contexto de la base de datos
+            dbContext.Database.MigrateAsync();
+
+            return app;
+        }
+    }
+}
