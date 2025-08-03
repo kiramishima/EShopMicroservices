239e340076be614379b0cd752ea0aa813f47201a
Kira Mishima <kiramishima@outlook.com>
2025-08-03 02:13:28 -0600

    Agregado StrongTyped Ids Fin Ordering.Domain

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class OrderItem : Entity<OrderItemId>
+    {
+        public OrderId OrderId { get; private set; }
+        public ProductId ProductId { get; private set; } = default!;
+        public int Quantity { get; private set; }
+        public decimal Price { get; private set; }
+        internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+        {
+            Id = OrderItemId.Of(Guid.NewGuid());
+            OrderId = orderId;
+            ProductId = productId;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
